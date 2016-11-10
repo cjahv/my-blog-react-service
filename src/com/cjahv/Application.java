fbd97e6a6965f0d7fe0efe6cfd6eda5a9edb5acd
jahv <cjahv@qq.com>
2016-11-10 16:37:15 +0800

    init

---
+++ src/com/cjahv/Application.java
@@ -0,0 +1,10 @@
+package com.cjahv;
+
+/**
+ * 入口
+ * Created by jahv on 2016/11/10.
+ */
+@EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.sshp.config", "com.linkcubic"})
+public class Application {
+}
