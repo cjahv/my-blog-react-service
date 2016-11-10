99b823745bdc277271f936301df89d83b4cecad2
jahv <cjahv@qq.com>
2016-11-10 17:43:37 +0800

    init

--- src/com/cjahv/Application.java
+++ src/com/cjahv/Application.java
@@ -1,10 +1,14 @@
 package com.cjahv;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * 入口
  * Created by jahv on 2016/11/10.
  */
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"com.sshp.config", "com.linkcubic"})
+@ComponentScan(basePackages = {"com.sshp.config", "com.cjahv"})
 public class Application {
+
 }
